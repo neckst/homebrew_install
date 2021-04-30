12e2f5ba2fc7be687e749a4b60686a03c2b4169a
Matthias Ecks <83242954+neckst@users.noreply.github.com>
2021-04-30 14:48:47 +0200

    Add files via upload

---
+++ brew_Apps_install.sh
@@ -0,0 +1,9 @@
+brew install git
+brew install plex
+brew install firefox
+brew install google-chrome
+brew install github
+brew install carbon-copy-cloner
+brew install plex-media-server
+brew install autopkgr 
+
